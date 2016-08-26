d959fb0206096d90b41072099bef1991351991c2
Amber <amberf0712@outlook.com>
2016-08-26 14:44:08 -0700

    Edit CSS, modify index.html to add in name entry, and update JS file to incorporate customer entered name

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,21 @@
 //Business Logic
-// function Pizza(topping, size) {
-//   this.topping = topping;
-//   this.size = size;
-// }
-//
-// Pizza.prototype.pizzaOrder = function() {
-//   return this.topping + ", " + this.size;
-// }
-//
-// function resetFields() {
-//   $(".topping").val("");
-//   $("#size").val("");
-// }
+function Pizza(topping, size) {
+  this.topping = topping;
+  this.size = size;
+}
+
+Pizza.prototype.pizzaOrder = function() {
+  return this.topping + ", " + this.size;
+}
+
+function isEmpty(value){
+    return (value === undefined || value == null || value.length <= 0) ? true : false;
+}
+
+function resetFields() {
+  $(".topping").val("");
+  $("#size").val("");
+}
 
 //User Interface Logic
 $(document).ready(function() {
@@ -21,13 +25,15 @@ $(document).ready(function() {
     event.preventDefault();
     var inputtedToppings = parseInt($("input:checkbox[name=toppings]:checked").val());
     var inputtedSize = parseInt($("#size").val());
-    var newOrder = (inputtedToppings + inputtedSize + "$");
+    var newOrder = (inputtedToppings + inputtedSize + "$ ");
+    var customerName = $("#yourName").val();
 
 
-
+    $("#orderHead").show();
     $("#orderInfo").show();
-    $("#orderInfo").text(newOrder);
+    $("#orderInfo").text(customerName + ", " + "Your order total is: " + newOrder  + "Have a nice day!");
     $("input#orderInfo").val("");
+
 
   });
 });
