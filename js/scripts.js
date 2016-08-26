b80ca33122743dd7f042214ed18302ebca674d49
Amber <amberf0712@outlook.com>
2016-08-26 11:45:03 -0700

    edit JS file for starting functionality with business and UI logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,32 @@
 //Business Logic
+function Pizza(topping, size) {
+  this.topping = topping;
+  this.size = size;
+}
 
+Pizza.prototype.pizzaOrder = function() {
+  return this.topping + ", " + this.size;
+}
+
+// function resetFields() {
+//   $(".topping").val("");
+//   $("#size").val("");
+// }
 
 //User Interface Logic
 $(document).ready(function() {
   $("#submitOrder").submit(event) {
-    
+    var inputtedToppings = parseInt($("input:radio[name=toppings]:checked").val());
+    var inputtedSize = parseInt($("#size").val());
+    var newOrder = (inputtedToppings + inputtedSize + "$");
 
+    if(inputtedToppings )
+    event.preventDefault();
+
+    $(".col-md-6").show();
+
+    $("#orderInfo").text(newOrder);
+    $("input#orderInfo").val("");
 
   });
 });
